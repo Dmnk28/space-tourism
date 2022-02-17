dd0c2746b42d943c418c6545539937230b43008b
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-02-17 08:09:57 +0100

    Startpage-Container adjusted

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -18,24 +18,24 @@ const Home: NextPage = () => {
         <Navigation />
       </header>
 
-      <main className='container text-white'>
-      
-        <h1 className='uppercase ff-sans-serif fs-500 text-light letter-spacing-1'> 
-          So, you want to travel to
-          <span className='text-white ff-serif fs-900'>Space</span>
-        </h1> 
-        
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely go to 
-          outer space and not hover kind of on the edge of it. Well sit back, and relax 
-          because we’ll give you a truly out of this world experience!
-        </p>
-
-        <Link href="/" passHref>
-          <a className='large-button uppercase ff-serif fs-600 text-dark bg-white'>Explore</a>
-        </Link> 
-      </main>
-
+      <div className='grid-container'>
+        <div>
+          <h1 className='uppercase ff-sans-serif fs-500 text-light letter-spacing-1'> 
+            So, you want to travel to
+            <span className='text-white ff-serif fs-900'>Space</span>
+          </h1> 
+          <p>
+            Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!
+          </p>
+        </div>
+        <div>
+          <Link href="/" passHref>
+            <a className='large-button uppercase ff-serif fs-600 text-dark bg-white'>Explore</a>
+          </Link> 
+        </div>
+      </div>
     </div>
   )
 }
