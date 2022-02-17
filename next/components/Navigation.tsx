5c07c57fd4d89d5af348c8229ddc4f655548af9f
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-02-17 07:16:47 +0100

    Corrections in the Navigation

--- next/components/Navigation.tsx
+++ next/components/Navigation.tsx
@@ -1,25 +1,31 @@
 import { NextComponentType } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 
 const Navigation: NextComponentType = () => {
     return (
-        <nav>
-            <ul className="primary-navigation underline-indicators flex">
-                <li>
-                    <Link href={'/'} passHref><a><span>00</span>Home</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
-                </li>
-                <li>
-                    <Link href={'/'} passHref><a><span>01</span>Destination</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
-                </li>
-                <li>
-                    <Link href={'/'} passHref><a><span>02</span>Crew</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
-                </li>
-                <li>
-                    <Link href={'/'} passHref><a><span>03</span>Technology</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
-                </li>
-            </ul>
+        <>
+            <div>
+                <Image src="/logo.svg" alt="space tourism logo" className="logo" width={30} height={30} />
+            </div>
+            <nav>
+                <ul className="primary-navigation underline-indicators flex">
+                    <li>
+                        <Link href={'/'} passHref><a className="uppercase text-white letter-spacing-2"><span>00</span>Home</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
+                    </li>
+                    <li>
+                        <Link href={'/destination'} passHref><a className="uppercase text-white letter-spacing-2"><span>01</span>Destination</a></Link>
+                    </li>
+                    <li>
+                        <Link href={'/crew'} passHref><a className="uppercase text-white letter-spacing-2"><span>02</span>Crew</a></Link> 
+                    </li>
+                    <li>
+                        <Link href={'/technology'} passHref><a className="uppercase text-white letter-spacing-2"><span>03</span>Technology</a></Link>
+                    </li>
+                </ul>
         </nav>
+        </>
     );
 }
 
