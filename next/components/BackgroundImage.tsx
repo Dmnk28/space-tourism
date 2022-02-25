e25a14c2740e13ae5a9413fc209523837637c112
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-02-25 19:51:55 +0100

    BackgroundImage now changing depending on Page and Device-Width

--- next/components/BackgroundImage.tsx
+++ next/components/BackgroundImage.tsx
@@ -1,24 +1,23 @@
-import { NextComponentType } from "next";
 import Image from "next/image";
+import useWindowSize from "../hooks/useWindowSize";
 
 type BgImg = {
     pageBgr: string;
 }
 
 const BackgroundImage: React.FC<BgImg> = ({ pageBgr }) => {
+    let windowWidth = useWindowSize()?.width;
+    let device      = windowWidth < 500 ? "mobile" : windowWidth < 1026 ? "tablet" : "desktop";
     
-    return (
-        
-            <Image 
-                src={`/${pageBgr}/background-${pageBgr}-desktop.jpg`}
-                // width={375}
-                // height={667}
-                layout="fill"
-                objectFit="cover" 
-                objectPosition="bottom center"
-                className='flex z-index-bgr'
-            />
-        )
+    return (        
+        <Image 
+            src={`/${pageBgr}/background-${pageBgr}-${device}.jpg`}
+            layout="fill"
+            objectFit="cover" 
+            objectPosition="bottom center"
+            className='flex z-index-bgr'
+        />
+    )
 }
 
-export default BackgroundImage
+export default BackgroundImage;
