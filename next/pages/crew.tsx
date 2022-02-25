2e25128d3c8a3305728a78cd8de66ffc4b64e387
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-02-25 18:58:17 +0100

    Created pages for crew and technology with Test-Content

---
+++ next/pages/crew.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Crew: NextPage = () => {
+    return (
+        <div>
+            Test
+        </div>
+    );
+}
+
+export default Crew;
---
+++ next/pages/technology.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Technology: NextPage = () => {
+    return (
+        <div>
+            Test
+        </div>
+    );
+}
+
+export default Technology;
