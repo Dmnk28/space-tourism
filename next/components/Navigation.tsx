716325544aa2f4936a7829d9cd34e866fbffa887
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-03-06 12:20:54 +0100

    Corrected the underline-indicators and Menu-logic
    Now the indicators sty in place if a submenu is actively in use. Made by using the aria-selected atrribute which has additional benefits fpr accesability

--- next/components/Navigation.tsx
+++ next/components/Navigation.tsx
@@ -19,8 +19,14 @@ const Navigation: React.FC<NavigationProps> = ({ setPageBgr }) => {
 
     const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>):void => {
         event.preventDefault();
+        /* get rid of old underline-decoration in Menu */
+        const allPrimaryNavLinkItems: NodeListOf<Element> = document.querySelectorAll('.p-nav-link');
+        allPrimaryNavLinkItems?.forEach(element => (element.ariaSelected = "false"));
+        /* BackgroundImageSetting & Routing */
+        setPageBgr(event.currentTarget.title);        
         router.push(event.currentTarget.href);
-        setPageBgr(event.currentTarget.title);
+        /* Set aria-Selected true for Screen-Readers and underline-styling(--> CSS) */
+        event.currentTarget.ariaSelected = "true";
     }
     
     const handleBurgerMenu = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>):void => {
@@ -49,16 +55,16 @@ const Navigation: React.FC<NavigationProps> = ({ setPageBgr }) => {
             <nav>
                 <ul id="primary-navigation" className="primary-navigation underline-indicators flex">
                     <li>
-                        <Link href={'/'} passHref><a className="uppercase text-white letter-spacing-2" title="home" onClick={handleClick}><span aria-hidden="true">00</span>Home</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
+                        <Link href={'/'} passHref><a className="p-nav-link uppercase text-white letter-spacing-2" title="home" onClick={handleClick}><span aria-hidden="true">00</span>Home</a></Link> {/* Link allows not more than one Child, so I use an a-element in combination with passHref */}
                     </li>
                     <li>
-                        <Link href={'/destination'} passHref><a className="uppercase text-white letter-spacing-2" title="destination" onClick={handleClick}><span aria-hidden="true">01</span>Destination</a></Link>
+                        <Link href={'/destination'} passHref><a className="p-nav-link uppercase text-white letter-spacing-2" title="destination" onClick={handleClick}><span aria-hidden="true">01</span>Destination</a></Link>
                     </li>
                     <li>
-                        <Link href={'/crew'} passHref><a className="uppercase text-white letter-spacing-2" title="crew" onClick={handleClick}><span aria-hidden="true">02</span>Crew</a></Link> 
+                        <Link href={'/crew'} passHref><a className="p-nav-link uppercase text-white letter-spacing-2" title="crew" onClick={handleClick}><span aria-hidden="true">02</span>Crew</a></Link> 
                     </li>
                     <li>
-                        <Link href={'/technology'} passHref><a className="uppercase text-white letter-spacing-2" title="technology" onClick={handleClick}><span aria-hidden="true">03</span>Technology</a></Link>
+                        <Link href={'/technology'} passHref><a className="p-nav-link uppercase text-white letter-spacing-2" title="technology" onClick={handleClick}><span aria-hidden="true">03</span>Technology</a></Link>
                     </li>
                 </ul>
             </nav>
--- next/pages/destination.tsx
+++ next/pages/destination.tsx
@@ -17,25 +17,31 @@ type Destinations = {
 const Destination: NextPage = () => {
     const [showDestination, setShowDestination] = useState(0);
     const destinations: Destinations = [...CONTENT.destinations]; 
-
+    
     const handleDestination = (index:number) => (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         event.preventDefault();
+        /* get rid of old underline-decoration in Menu */
+        const allTabItems: NodeListOf<Element> = document.querySelectorAll('.tab');
+        allTabItems?.forEach(element => (element.ariaSelected = "false"));
+        /* Trigger Content-Change*/
         setShowDestination(index);
+        /* Set aria-Selected true for Screen-Readers and underline-styling(--> CSS) */
+        event.currentTarget.ariaSelected = "true";
     }
 
     return (
         <main className='grid-container grid-container--destination z-1'>
             <div>
-                <h2 className="numbered-title"><span>01</span> Pick your destination</h2>
+                <h1 className="numbered-title"><span aria-hidden="true">01</span> Pick your destination</h1>
                 <div className="destination-image" >
-                    <Image src={destinations[showDestination].images.webp} width={445} height={445} layout="intrinsic"></Image>
+                    <Image src={destinations[showDestination].images.webp} alt={`the ${destinations[showDestination].name}`} width={445} height={445} layout="intrinsic"></Image>
                 </div>
             </div>
             <div>
                 <div className="tab-list underline-indicators flex ">
                     {destinations.map((element, index) => {
                         return (
-                            <a key={element.name} className="uppercase ff-sans-condensed text-accent letter-spacing-2" onClick={handleDestination(index)}>{element.name}</a>
+                            <a key={element.name} className="tab uppercase ff-sans-condensed text-accent letter-spacing-2" onClick={handleDestination(index)}>{element.name}</a>
                         )
                     })}
                 </div>
