22e01857c65e30ae33ff0a413708ee489f9e8d25
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-04-04 10:32:30 +0200

    Implemented logic and structured layout of technology-page

--- next/pages/technology.tsx
+++ next/pages/technology.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import useWindowSize from "../hooks/useWindowSize";
-
 import { NextPage } from "next";
 import Image from "next/image";
+
+import useWindowSize from "../hooks/useWindowSize";
+import switchActiveLookingTabIndicators from "../utils/switchActiveLookingTabIndicators";
 
 import { CONTENT } from "../data/content";
 
@@ -15,6 +16,7 @@ const Technology: NextPage = () => {
 
     const changeSelectedTech = (index: number) => (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.preventDefault();
+        switchActiveLookingTabIndicators('.numbered-dot-btn', event.currentTarget);
         setCurrentTech(tech[index]);
     }
 
@@ -29,24 +31,28 @@ const Technology: NextPage = () => {
 
     return (
         <main className="grid-container grid-container--tech z-1 flow">
-            <h1 className="numbered-title"><span className="sr-only">03</span>Space Launch 101</h1>
-            <div className="numbered-dots flex">
-                {
-                    tech.map((techElement, index) => {
-                        return (
-                            <button onClick={changeSelectedTech(index)}>{index + 1}<span className="sr-only">{techElement.name}</span></button>
-                        )
-                    })
-                }
+            <h1 className="numbered-title"><span aria-hidden="true">03</span>Space Launch 101</h1>
+            <div className="tech-tabs flow">
+                <div className="numbered-dots flex">
+                    {
+                        tech.map((techElement, index) => {
+                            return (
+                                <button key={techElement.name} onClick={changeSelectedTech(index)} className="numbered-dot-btn">
+                                    {index + 1}
+                                    <span className="sr-only">{techElement.name}</span>
+                                </button>
+                            )
+                        })
+                    }
+                </div>
+                <article className="tech-content flow">
+                    <header className="flow flow--space-small">
+                        <p className="uppercase">The Terminology...</p>
+                        <h2 className="fs-700 ff-serif uppercase">{currentTech.name}</h2>
+                    </header>
+                    <p>{currentTech.description}</p>
+                </article>
             </div>
-            <article className="tech-content flow">
-                <header className="flow flow--space-small">
-                    <p className="uppercase">The Terminology...</p>
-                    <h2 className="fs-700 ff-serif uppercase">{currentTech.name}</h2>
-                </header>
-                <p>{currentTech.description}</p>
-            </article>
-
             <div className="tech-image">
                 {selectImage()}
             </div>
